39bd86c8ab16ef5c2310e5338f203713531b0199
Mladen Yakimov <yakimov89@gmail.com>
2014-11-25 00:09:37 -0500

    Initial commit Generate Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeerGuideDataVisualization::Application.config.secret_key_base = 'd25522a4ee99dacd4fabc72eb9e4be005ec2d0e8079430eaeae2f7181489e7a6f94eb34de537ee37d8caad4130a980d7530e8e31e91d62d05529732eebd8fd4e'
